4e18c297860f67e9bfef63052278073d38a04e9b
RIZAM <rijarmali@gmail.com>
2026-06-09 17:17:19 +0700

    update

--- Geometri/prisma_segitiga.js
+++ Geometri/prisma_segitiga.js
@@ -2,15 +2,24 @@ const prompt = require("prompt-sync")({signit: true});
 
 // Volume prisma segitiga
 console.log("=== volume prisma segitiga ===");
-let l_alas = parseFloat(prompt("Masukkan luas alas segitiga: "));
+let alas = parseFloat(prompt("Masukkan alas segitiga: "));
+let t_segitiga = parseFloat(prompt("Masukkan tinggi segitiga: "));
 let t_prisma = parseFloat(prompt("Masukkan tinggi prisma: "));
-let v = 1/2  * l_alas * t_prisma;
+let v = 0.5  * alas * t_segitiga* t_prisma;
 console.log("volume adalah: " + v);
 
 // Luas permukaan prisma segitiga
 console.log("\n=== Luas permukaan prisma segitiga ===");
-let l_alas2 = parseFloat(prompt("Masukkan luas alas segitiga: "));
-let t_prisma2 = parseFloat(prompt("Masukkan tinggi prisma: "));
-let k_prisma = parseFloat(prompt("Masukkan keliling prisma: "));
-let lp = (2 * l_alas2) + (k_prisma * t_prisma);
-console.log("Luas permukaan prisma segitiga adalah: " + lp);
+let alas2 = parseFloat(prompt("Masukkan alas segitiga: "));
+let tinggisegitiga = parseFloat(prompt("Masukkan tinggi segitiga: "));
+
+let sisi1 = parseFloat(prompt("Masukkan sisi: "));
+let sisi2 = parseFloat(prompt("Masukkan sisi: "));
+let sisi3 = parseFloat(prompt("Masukkan sisi: "));
+
+let tinggiprisma = parseFloat(prompt("Masukkan tinggi prisma: "));
+
+let luasalas = 0.5 * alas2 * tinggisegitiga;
+let kelilingalas = sisi1 + sisi2 + sisi3;
+let luaspermukaan = ( 2 * luasalas) + (kelilingalas * tinggiprisma);
+console.log("Luas permukaan prisma segitiga adalah: " + luaspermukaan);
